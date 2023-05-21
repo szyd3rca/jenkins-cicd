7fff7a78fd6bb373b901c868b8a62dda79887f37
Piotr <fanbule@o2.pl>
2023-05-21 20:49:22 +0200

    changed to login using local profile

--- provider.tf
+++ provider.tf
@@ -1,5 +1,8 @@
 provider "aws" {
   region     = "eu-central-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
+  #access_key = var.access_key
+  #secret_key = var.secret_key
+  profile = "terraform"
+  shared_credentials_file =  "~/.aws/credentials"
 }
+
