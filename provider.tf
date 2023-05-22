6f14cea4788260e2d1d55a4cb379fd3cc1a4cf00
Piotr <fanbule@o2.pl>
2023-05-22 16:38:10 +0200

    fix: naprawa polaczenia ssh

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region = "eu-central-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
-  #profile                 = "terraform"
-  #shared_credentials_file = "~/.aws/credentials"
+  #access_key = var.access_key
+  #secret_key = var.secret_key
+  profile                 = "terraform"
+  shared_credentials_file = "~/.aws/credentials"
 }
 
